7dbeaec29b221b4973d712251ab1bb633122cf8f
duedull <mr.duedull@gmail.com>
2026-03-14 22:19:03 +0530

    changes done

---
+++ src/series/StringClass.java
@@ -0,0 +1,14 @@
+package series;
+
+public class StringClass {
+    static void main() {
+        String ab = "this is a string";
+        ab = "banger";
+        String bc = "banger";
+        System.out.println(ab.equals(bc));
+        System.out.println(ab.length());
+        System.out.println(ab.charAt(3));
+
+
+    }
+}
